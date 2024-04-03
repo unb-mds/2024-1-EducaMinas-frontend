ec43a23df668debb7602c35885a13f02168b24f3
rafgpereira <rafaelgomespereira123@gmail.com>
2024-04-03 11:19:09 -0300

    style(#15): includes eslint configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Squad 05",
-  description: "Trabalho de MDS",
+  title: 'Squad 05',
+  description: 'Trabalho de MDS',
 };
 
 export default function RootLayout({
