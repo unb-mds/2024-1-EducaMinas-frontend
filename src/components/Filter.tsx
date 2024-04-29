4604fde35319863e9154817c71da4c989ac2bd28
Joao Lucas <joaoaraujosiqueira@gmail.com>
2024-04-29 08:59:11 -0300

    style(#49): includes filter style

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,10 +1,10 @@
 export default function Filter(props: { options: string[]; label: string }) {
   return (
     <div className="flex flex-col">
-      <label>{props.label}</label>
-      <select>
+      <label className="font-bold mb-1">{props.label}</label>
+      <select className="border rounded-lg p-2 bg-primary-red text-primary-white">
         {props.options.map((option, index) => (
-          <option key={index} value={option}>
+          <option key={index} value={option} style={{ backgroundColor: 'white', color: 'black' }}>
             {option}
           </option>
         ))}
