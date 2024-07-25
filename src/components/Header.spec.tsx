9154cc3aaec72ffb0ccd1972b378bbeb687fe38f
rafgpereira <rafaelgomespereira123@gmail.com>
2024-07-24 21:47:19 -0300

    tests#80: includes header unit tests

---
+++ src/components/Header.spec.tsx
@@ -0,0 +1,37 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import { describe, expect, test, vi } from 'vitest';
+import Header from './Header';
+
+const mockRouter = vi.fn();
+vi.mock('next/router', () => ({
+  useRouter: () => mockRouter,
+}));
+
+describe('Header Component', () => {
+  test('should render the logo and title', () => {
+    render(<Header />);
+
+    expect(screen.getByAltText('logo')).toBeInTheDocument();
+    expect(screen.getByText('EDUCA')).toBeInTheDocument();
+    expect(screen.getByText('MINAS')).toBeInTheDocument();
+  });
+
+  test('should render the "Sobre" button', () => {
+    render(<Header />);
+    const sobreButtons = screen.getAllByText(/Sobre/i);
+    expect(sobreButtons.length).toBeGreaterThan(0);
+  });
+
+  test('should render the "Início" button', () => {
+    render(<Header />);
+    const inicioButtons = screen.getAllByText(/Início/i);
+    expect(inicioButtons.length).toBeGreaterThan(0);
+  });
+
+  test('should render the "Pesquisa" button', () => {
+    render(<Header />);
+    const pesquisaButtons = screen.getAllByText(/Pesquisa/i);
+    expect(pesquisaButtons.length).toBeGreaterThan(0);
+  });
+});
