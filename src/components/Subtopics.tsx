245ec2d3864b4b3399ebdec435472d0063ff8329
João Pedro <joaopedrosilvasousa003@gmail.com>
2024-07-26 11:24:00 -0300

    fix(#78):fixing subtopics responsiveness

--- src/components/Subtopics.tsx
+++ src/components/Subtopics.tsx
@@ -2,8 +2,8 @@
 export default function Subtopics(props: { title: string; text: string }) {
   return (
     <div className="w-full text-center items-center flex flex-col gap-2 my-3 primary-gray border-t-2 ">
-      <h2 className="font-[700] text-[1.4em] pt-5 whitespace-nowrap">{props.title}</h2>
-      <h3 className="text-[1.1em] font-[600] text-center w-full w-[90vw] pr-[5px] pl-[5px]">{props.text}</h3>
+      <h2 className="font-[700] text-[1.3em] pt-5 whitespace-nowrap sm:text-[1.4em]">{props.title}</h2>
+      <h3 className="text-[1.1em] font-[600] text-center w-full w-[98vw] pr-[5px] pl-[5px]">{props.text}</h3>
     </div>
   );
 }
