ee7e505f79b0686efb739fa7d48c56a7768d4ece
João Pedro <joaopedrosilvasousa003@gmail.com>
2024-07-26 11:57:09 -0300

    fix(#78):finalizing last changes

--- src/components/Subtopics.tsx
+++ src/components/Subtopics.tsx
@@ -3,7 +3,7 @@ export default function Subtopics(props: { title: string; text: string }) {
   return (
     <div className="w-full text-center items-center flex flex-col gap-2 my-3 primary-gray border-t-2 ">
       <h2 className="font-[700] text-[1.3em] pt-5 whitespace-nowrap sm:text-[1.4em]">{props.title}</h2>
-      <h3 className="text-[1.1em] font-[600] text-center w-full w-[98vw] pr-[5px] pl-[5px]">{props.text}</h3>
+      <h3 className="text-[0.9em] font-[600] text-center w-full w-[97vw] pr-[5px] pl-[5px]">{props.text}</h3>
     </div>
   );
 }
--- src/components/Topics.tsx
+++ src/components/Topics.tsx
@@ -3,7 +3,7 @@ export default function Topics(props: { title: string; text: string }) {
   return (
     <div className=" w-full text-center items-center flex flex-col gap-2 my-3 primary-gray">
       <h1 className="font-extrabold sm:text-[1.5em] text-[1.4em] whitespace-nowrap">{props.title}</h1>
-      <h3 className="text-[1.1em] font-[600] text-center w-full w-[98vw] pb-2">{props.text}</h3>
+      <h3 className="text-[1.1em] font-[600] text-center w-full w-[97vw] pb-2">{props.text}</h3>
     </div>
   );
 }
