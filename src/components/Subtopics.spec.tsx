a56c6624f721ee8de9fa27631215059e65d0fc08
Joao Lucas <joaoaraujosiqueira@gmail.com>
2024-08-07 22:07:14 -0300

    test(#101):Includes new tests for subtopics component

--- src/components/Subtopics.spec.tsx
+++ src/components/Subtopics.spec.tsx
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { describe, expect, test, vi } from 'vitest';
 import Subtopics from './Subtopics';
 
@@ -16,15 +16,39 @@ describe('Subtopics Component', () => {
     const textElement = screen.getByText(/Test Text/i);
     expect(textElement).toBeInTheDocument();
   });
+
   test('applies the correct classes', () => {
     render(<Subtopics title="Test Title" text="Test Text" />);
     const containerElement = screen.getByText(/Test Title/i).parentElement;
-    expect(containerElement).toHaveClass(
-      'w-full text-center items-center flex flex-col gap-2 my-3 primary-gray border-t-2',
-    );
+    expect(containerElement).toHaveClass('flex flex-row');
+
     const titleElement = screen.getByText(/Test Title/i);
     expect(titleElement).toHaveClass('font-[700] text-[1.3em] pt-5 whitespace-nowrap sm:text-[1.4em]');
     const textElement = screen.getByText(/Test Text/i);
     expect(textElement).toHaveClass('text-[0.9em] font-[600] text-center w-[97vw] pr-[5px] pl-[5px]');
+  });
+
+  test('opens and closes the popup when the Info button and X button are clicked', () => {
+    render(<Subtopics title="Test Title" text="Test Text" />);
+
+    // Encontra o botão Info pelo elemento SVG
+    const infoButton = screen.getByRole('button');
+    fireEvent.click(infoButton);
+
+    // Verifica se o Popup está no documento após o clique no Info
+    const popup = screen.queryByTestId('popup');
+    expect(popup).toBeInTheDocument();
+
+    // Simula o clique no botão X do Popup para fechar o Popup
+    const closeButton = screen.getByTestId('close-button'); // Usa o data-testid para encontrar o botão de fechar
+    fireEvent.click(closeButton);
+
+    // Verifica se o Popup foi fechado
+    expect(screen.queryByTestId('popup')).not.toBeInTheDocument();
+  });
+  test('does not render the popup when isOpen is false', () => {
+    render(<Subtopics title="Test Title" text="Test Text" />);
+
+    expect(screen.queryByTestId('popup')).not.toBeInTheDocument();
   });
 });
