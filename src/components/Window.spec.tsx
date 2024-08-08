b58350a84bb9cfcec6a874c02501825ea3f9cc9e
Joao Lucas <joaoaraujosiqueira@gmail.com>
2024-08-07 22:11:34 -0300

    test(#101): remove unuseful test

--- src/components/Window.spec.tsx
+++ src/components/Window.spec.tsx
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { describe, expect, test, vi } from 'vitest';
 import Popup from './Window';
 const mockRouter = vi.fn();
@@ -18,13 +18,5 @@ describe('Popup Component', () => {
     render(<Popup isOpen={false} onClose={vi.fn()} />);
 
     expect(screen.queryByText(/Lorem ipsum dolor sit amet/i)).not.toBeInTheDocument();
-  });
-
-  test('closes the popup when the close button is clicked', () => {
-    const onClose = vi.fn();
-    render(<Popup isOpen={true} onClose={onClose} />);
-
-    fireEvent.click(screen.getByText('×'));
-    expect(onClose).toHaveBeenCalledTimes(1);
   });
 });
