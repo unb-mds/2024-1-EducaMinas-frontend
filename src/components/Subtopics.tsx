e6f1d332c96acbc31ed47785f2dd3deaed855f02
João Pedro <joaopedrosilvasousa003@gmail.com>
2024-08-12 12:18:18 -0300

    style(#105): cleaning code

--- src/components/Subtopics.tsx
+++ src/components/Subtopics.tsx
@@ -19,8 +19,8 @@ export default function Subtopics(props: { title: string; text: string; Popuptex
     <div className="w-full text-center items-center flex flex-col gap-2 my-3 primary-gray border-t-2 ">
       <div className="flex flex-row">
         <h2 className="font-[700] text-[1.3em] pt-5 whitespace-nowrap sm:text-[1.4em]">{props.title}</h2>
-        <button className="mt-4 ml-3" onClick={handleOpen}>
-          <Info size={24} />
+        <button className="mt-5 ml-3" onClick={handleOpen}>
+          <Info size={20} />
         </button>
       </div>
       <h3 className="text-[0.9em] font-[600] text-center w-[97vw] pr-[5px] pl-[5px]">{props.text}</h3>
