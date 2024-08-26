6efe490b598728dec3b63000554f1eceab15468b
João Pedro <joaopedrosilvasousa003@gmail.com>
2024-08-25 22:00:21 -0300

    feat(#126): changing home path and adding icon

---
+++ src/app/home/page.spec.tsx
@@ -0,0 +1,30 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import { describe, expect, test, vi } from 'vitest';
+import Home from './page';
+
+const mockRouter = vi.fn();
+vi.mock('next/router', () => ({
+  useRouter: () => mockRouter,
+}));
+
+describe('Home Component', () => {
+  test('Should render Home page', () => {
+    render(<Home />);
+
+    expect(screen.getByText('Investigue os dados sobre a Educação do Estado de Minas Gerais')).toBeInTheDocument();
+    expect(
+      screen.getByText(
+        'Visualize de forma intuitiva, em gráficos, informações da educação do estado de Minas Gerais extraídas da pesquisa educacional realizada pelo INEP.',
+      ),
+    ).toBeInTheDocument();
+    expect(screen.getByAltText('home')).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Descubra mais' })).toBeInTheDocument();
+  });
+
+  test('Should render Button and link to the correct page', () => {
+    render(<Home />);
+    const button = screen.getByText(/Descubra mais/i);
+    expect(button.closest('a')).toHaveAttribute('href', '/search');
+  });
+});
