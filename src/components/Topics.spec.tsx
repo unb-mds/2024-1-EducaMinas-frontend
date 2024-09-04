649d86ae11d0ed4dbad59ff0b0aee2de3da8245c
Joao Lucas <joaoaraujosiqueira@gmail.com>
2024-09-03 23:13:03 -0300

    test(#151): Changes Topics test

--- src/components/Topics.spec.tsx
+++ src/components/Topics.spec.tsx
@@ -21,7 +21,7 @@ describe('Topics Component', () => {
     const containerElement = screen.getByText(/Test Title/i).parentElement;
     expect(containerElement).toHaveClass('w-full text-center items-center flex flex-col gap-2 my-3 primary-gray');
     const titleElement = screen.getByText(/Test Title/i);
-    expect(titleElement).toHaveClass('font-extrabold sm:text-[1.5em] text-[1.4em] whitespace-nowrap');
+    expect(titleElement).toHaveClass('font-extrabold sm:text-[2em] text-[1.6em] whitespace-nowrap');
     const textElement = screen.getByText(/Test Text/i);
     expect(textElement).toHaveClass('text-[1.1em] font-[600] text-center w-[97vw] pb-2');
   });
